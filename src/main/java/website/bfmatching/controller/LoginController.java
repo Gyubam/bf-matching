646a043da15cd43c2f59b0c134087af75d3d9fe2
서규범 <sgb8170@naver.com>
2022-09-04 22:01:11 +0900

    회원가입 페이지 디자인

--- src/main/java/website/bfmatching/controller/LoginController.java
+++ src/main/java/website/bfmatching/controller/LoginController.java
@@ -1,17 +1,21 @@
 package website.bfmatching.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
-public class MemberController {
+@RequiredArgsConstructor
+public class LoginController {
 
-    //회원가입
-    @GetMapping("/members/new")
-    public String addForm() {
 
-        return "/layout/login/addMemberForm";
+    //로그인 폼
+    @GetMapping("/login")
+    public String loginForm() {
+
+        return "/layout/login/loginForm";
     }
+
 
 }
