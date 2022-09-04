f45cbea1e4a624f8d5f7ba02dc9063ad614a6089
서규범 <sgb8170@naver.com>
2022-09-04 16:16:56 +0900

    Initial commit

---
+++ src/main/java/website/bfmatching/BfMatchingApplication.java
@@ -0,0 +1,13 @@
+package website.bfmatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfMatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfMatchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/website/bfmatching/BfMatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package website.bfmatching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfMatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
